b2c730a0c0ad26c033665a3ac3e7e101a011537a
himanshu-orsolutions <himanshu.optiroutesolutions@gmail.com>
2020-03-15 22:44:34 +0530

    initial commit

---
+++ src/main/java/com/scrap/fitinpartscrapper/FitinpartScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.scrap.fitinpartscrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitinpartScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitinpartScrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scrap/fitinpartscrapper/FitinpartScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrap.fitinpartscrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitinpartScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
