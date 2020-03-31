5915670bdc05007c1515858c7803a76912b5fba9
himanshu-orsolutions <himanshu.optiroutesolutions@gmail.com>
2020-03-31 17:27:08 +0530

    add cron script

---
+++ cron.sh
@@ -0,0 +1,8 @@
+word_count=$( jps | grep FitinpartScrapperApplication | wc -w )
+if [ $word_count -eq 0 ]
+then
+	echo "starting application again" >> cron_log.txt
+	mvn spring-boot:run &
+else
+	echo "Application is running" >> cron_log.txt
+fi
