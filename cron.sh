cc31012153f8da9a1e5f516fe3484e932b24a713
himanshu-orsolutions <himanshu.optiroutesolutions@gmail.com>
2020-04-01 21:01:55 +0530

    update cron script

--- cron.sh
+++ cron.sh
@@ -2,7 +2,8 @@ word_count=$( jps | grep FitinpartScrapperApplication | wc -w )
 if [ $word_count -eq 0 ]
 then
 	echo "starting application again" >> cron_log.txt
-	mvn spring-boot:run &
+	cd /home/ubuntu/fitinpart-scrapper
+	mvn spring-boot:run &	
 else
 	echo "Application is running" >> cron_log.txt
 fi
